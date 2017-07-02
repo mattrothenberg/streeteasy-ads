38b5607ca894a4c546fce97c9d30c9c0b5ccb790
Matt Rothenberg <mattrothenberg@gmail.com>
2017-07-02 12:55:43 -0400

    Add github icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app" class="vh-100">
+    <div class="fixed top-1 right-1" v-show="showingExportButton">
+      <a class="github-button" href="https://github.com/mattrothenberg/streeteasy-ads" data-icon="octicon-star" data-size="small" data-show-count="true" aria-label="Star mattrothenberg/streeteasy-ads on GitHub">Star</a>
+      <a class="github-button" href="https://github.com/mattrothenberg/streeteasy-ads/fork" data-icon="octicon-repo-forked" data-size="small" data-show-count="true" aria-label="Fork mattrothenberg/streeteasy-ads on GitHub">Fork</a>
+    </div>
     <div class="h-100 flex flex-column items-center justify-center">
       <advertisement :copy="copy"></advertisement>
       <div class="mt4">
-        <button @click="exportImage" v-if="showingExportButton" class="bn f6 dim br1 ph3 pv2 mb2 white bg-black fw7 ttu tracked pointer">Export Image</button>
+        <button @click="exportImage" v-show="showingExportButton" class="bn f6 dim br1 ph3 pv2 mb2 white bg-black fw7 ttu tracked pointer">Export Image</button>
       </div>
     </div>
   </div>
